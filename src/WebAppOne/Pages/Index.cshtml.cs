0b408bbb584a997fef98c5c1aff888941b678c5e
Herb Stahl <ghstahl@gmail.com>
2019-08-14 07:55:28 -0700

    Added Request dump to index page

--- src/WebAppOne/Pages/Index.cshtml.cs
+++ src/WebAppOne/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,15 @@ namespace WebAppOne.Pages
 {
     public class IndexModel : PageModel
     {
+      
+        public HttpRequest Request { get; set; }
+        public IndexModel( )
+        {
+           
+        }
         public void OnGet()
         {
-
+            Request = HttpContext.Request;
         }
     }
 }
